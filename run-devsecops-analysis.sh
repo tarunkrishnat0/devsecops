d76b9d155feeab56e5124e040758cd0b41e77cb8
Tarun Krishna T <tarunkrishna.t0@gmail.com>
2024-10-25 15:45:02 +0530

    Fixed invalid repo path

--- run-devsecops-analysis.sh
+++ run-devsecops-analysis.sh
@@ -26,18 +26,18 @@ generate_report() {
     echo "-- Supply Chain Security report is at reports/$repo_name/${repo_name}_supply_chain.csv"
 
     # Bandit is an open-source SAST tool designed specifically for Python applications.
-    docker run --rm -v $(pwd)/$repo_path:/src -v $(pwd)/reports:/reports ghcr.io/pycqa/bandit/bandit -r /src -f json -o /reports/$repo_name/tool_outputs/${repo_name}_bandit.json
+    docker run --rm -v $repo_path:/src -v $(pwd)/reports:/reports ghcr.io/pycqa/bandit/bandit -r /src -f json -o /reports/$repo_name/tool_outputs/${repo_name}_bandit.json
 
     # Horusec is an open-source tool that performs a static code analysis to identify security flaws during development.
     # Current languages for analysis are C#, Java, Kotlin, Python, Ruby, Golang, Terraform, Javascript, Typescript, Kubernetes, PHP, C, HTML, JSON, Dart, Elixir, Shell, and Nginx.
-    docker run -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd)/$repo_path:/src -v $(pwd)/reports:/reports horuszup/horusec-cli:v2.9.0-beta.3 horusec start -p /src -P $(pwd) -o json -O /reports/$repo_name/tool_outputs/${repo_name}_horusec.json
+    docker run -v /var/run/docker.sock:/var/run/docker.sock -v $repo_path:/src -v $(pwd)/reports:/reports horuszup/horusec-cli:v2.9.0-beta.3 horusec start -p /src -P $(pwd) -o json -O /reports/$repo_name/tool_outputs/${repo_name}_horusec.json
 
     # Bearer is a static application security testing (SAST) tool that scans your source code and analyzes your data flows to discover, filter, and prioritize security and privacy risks.
     # Currently supports JavaScript, TypeScript, Ruby, and Java stacks.
     bearer_output="reports/$repo_name/tool_outputs/${repo_name}_bearer.json" 
     touch $bearer_output
     chmod 666 $bearer_output
-    echo "docker run --rm -v $(pwd)/$repo_path:/src -v $(pwd)/reports:/reports -v $(pwd)/configs/bearer-config.yml:/bearer-config.yml -v $(pwd)/configs/bearer-rules:/rules bearer/bearer:latest-amd64 scan /src --config-file /bearer-config.yml -f json --output /$bearer_output"
+    echo "docker run --rm -v $repo_path:/src -v $(pwd)/reports:/reports -v $(pwd)/configs/bearer-config.yml:/bearer-config.yml -v $(pwd)/configs/bearer-rules:/rules bearer/bearer:latest-amd64 scan /src --config-file /bearer-config.yml -f json --output /$bearer_output"
 }
 
 # for repo_path in $1* ; do
