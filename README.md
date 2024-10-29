08b048165f865f596c524c5067e4b41088fd2386
Tarun Krishna T <tarunkrishna.t0@gmail.com>
2024-10-29 13:11:09 +0530

    Working license finder with pip caching

--- README.md
+++ README.md
@@ -1,10 +1,21 @@
+# Intro
+- Tested on Ubuntu 24.04
+- Create a virtual env and install the pip requirements.
+
+Clone the dev branch with all submodules.
 ```sh
-# Apply patches to submodules
 git clone --recursive -b dev git@github.com:tarunkrishnat0/devsecops.git
+```
+
+# Setting up third party tools
+
+## Defect Dojo
+### Setup
+```sh
+# Apply a patch that will fix bearer scan results import error
 cd configs/django-DefectDojo/
 git apply  ../django-DefectDojo.patch
 
-################ Defect Dojo #######################
 # Building Docker images
 ./dc-build.sh
 
@@ -14,6 +25,26 @@ git apply  ../django-DefectDojo.patch
 
 # Obtain admin credentials. The initializer can take up to 3 minutes to run.
 # Use docker compose logs -f initializer to track its progress.
-docker compose logs initializer | grep "Admin password:"
-
+docker compose logs initializer | grep "Admin password:" | tee defect_dojo_creds.txt
 ```
+
+### Creating user for importing scan results
+Create a user in defect dojo as per below details:
+```
+username: scan-importer
+password: Scan-Importer09
+role: API-Importer
+```
+Note: These creds should be same as `./config/env`
+
+### Creating Product and engagments
+- Create a product in defect dojo with name as per your need.
+- Create engagements under the product with name as the repo folder name (that is given for scanning). Scan results of a repo are uploaded to the engagement that has same name as the folder name.
+
+# Execution
+```sh
+time bash run-devsecops-analysis.sh /full/path/to/project/
+```
+
+# Scan Results
+Scan results are stored under `./reports/` with same folder name as the project folder.
--- run-devsecops-analysis.sh
+++ run-devsecops-analysis.sh
@@ -29,9 +29,11 @@ generate_report() {
 
     # Supply Chain Analysis
     echo -e "\n ########### Running Supply Chain Analysis ########### \n"
-    syft scan dir:$repo_path -q -c configs/syft.yaml -o cyclonedx-json=reports/$repo_name/sbom/${repo_name}_${latest_commit_id}_sbom_cyclonedx.json
+    echo -e "\n ########### Running Syft ########### \n"
+    time syft scan dir:$repo_path -c configs/syft.yaml -o cyclonedx-json=reports/$repo_name/sbom/${repo_name}_${latest_commit_id}_sbom_cyclonedx.json
     # syft scan dir:$repo_path -q -o json > reports/$repo_name/sbom/${repo_name}_${latest_commit_id}_sbom.json
-    grype sbom:reports/$repo_name/sbom/${repo_name}_${latest_commit_id}_sbom_cyclonedx.json -q -o json=reports/$repo_name/tool_outputs/${repo_name}_${latest_commit_id}_grype.json
+    echo -e "\n ########### Running Grype ########### \n"
+    time grype sbom:reports/$repo_name/sbom/${repo_name}_${latest_commit_id}_sbom_cyclonedx.json -o json=reports/$repo_name/tool_outputs/${repo_name}_${latest_commit_id}_grype.json
     python3 create-csv-from-vuln-json.py reports/$repo_name/tool_outputs/${repo_name}_${latest_commit_id}_grype.json reports/$repo_name/${repo_name}_${latest_commit_id}_supply_chain.csv
 
     if [ -f $repo_path/requirements.txt ]; then
@@ -40,13 +42,75 @@ generate_report() {
 
         cd $repo_path/
 
-        time licensecheck -u 'requirements:requirements.txt;requirements_optional.txt' -f csv -o $licensecheck_output
+        # requirements_pattern="requirements.*\.txt$"
+        requirements_files=""
+        pip_packages_to_install=""
+        for file in ./requirements*.txt; do
+            # Check if the file name matches the regex pattern
+            if [[ -f "$file" ]]; then
+                # echo "Processing file: $file"
+                cat $file | grep -e '^-r ' > /dev/null 2>&1
+                if [[ $? -ne 0 ]]; then
+                    requirements_files+="$(basename $file);"
+                    while IFS= read -r line; do
+                        # Skip lines that start with "ib-" or "ar-"
+                        if [[ "$line" == ib-* || "$line" == ib_* || "$line" == django-swagger-utils*  || "$line" == s3_uploader* || "$line" == \#* ]]; then
+                            echo "Ignoring package $line"
+                            continue
+                        fi
+                        # Append the line to the combined_requirements string with a newline
+                        pip_packages_to_install+="$line"$'\n'
+                    done < "$file"
+                else
+                    echo "Ignoring file: $file"
+                fi
+            fi
+        done
+        requirements_files="${requirements_files%;}"
+        echo $requirements_files
 
+        echo -e "\n ########### Running licensecheck ########### \n"
+
+        time licensecheck -u "requirements:$requirements_files" -f csv -o $licensecheck_output
+
+        echo -e "\n ########### Running licensefinder ########### \n"
         license_finder_columns="name version licenses approved package_manager summary install_path"
-        time docker run --rm -v $PWD:/scan -v $devsecops_folder_path/reports/:/reports -v $devsecops_folder_path/configs/:/configs licensefinder/license_finder /bin/bash -lc "cd /scan && pip install -r requirements.txt && license_finder report --decisions-file=/configs/LicenseFinder/dependency_decisions.yml --format=csv --columns=$license_finder_columns | tail -n +2 | tee /$license_finder_output_path_relative"
+        requirements_files_install=${requirements_files//";"/" -r "}
+        echo $requirements_files_install
+        echo $pip_packages_to_install
+        # Pipe the combined requirements to pip for installation
+        # Only if there is any content in combined_requirements
+        if [[ -n "$pip_packages_to_install" ]]; then
+            mv -f requirements.txt requirements_original_backup_${latest_commit_id}.txt
+            echo "$pip_packages_to_install" > requirements.txt
+            echo "$pip_packages_to_install" > combined_requirements_${latest_commit_id}.txt
+            if [ -d .venv ]; then
+                mv -T .venv .venv_${latest_commit_id}
+            fi
+            time docker run --rm -v $PWD:/scan \
+                -v $devsecops_folder_path/reports/:/reports \
+                -v $devsecops_folder_path/configs/:/configs \
+                -v ~/.aws:/root/.aws -v ~/.profile:/profile \
+                -v $devsecops_folder_path/.cache/license_finder/pip:/root/.cache/pip:rw \
+                licensefinder/license_finder \
+                /bin/bash -lc "cd /scan && apt update > /dev/null && apt install -y libmysqlclient-dev awscli python3.10-venv > /dev/null && chown -R root:root ~/.cache/pip && source /profile && python3 -m venv .venv && source .venv/bin/activate && pip install -r requirements.txt > /dev/null && license_finder report --decisions-file=/configs/LicenseFinder/dependency_decisions.yml --format=csv --columns=$license_finder_columns | grep -v "LicenseFinder::*" | tee /$license_finder_output_path_relative > /dev/null"
+            sudo rm -rf .venv
+            # time docker run --rm -v $PWD:/scan -v $devsecops_folder_path/reports/:/reports -v $devsecops_folder_path/configs/:/configs licensefinder/license_finder /bin/bash -lc "cd /scan && apt update && apt install -y libmysqlclient-dev && pip install -r $requirements_files_install && license_finder report --decisions-file=/configs/LicenseFinder/dependency_decisions.yml --format=csv --columns=$license_finder_columns | tail -n +2 | tee /$license_finder_output_path_relative"
+            
+            mv -f requirements_original_backup_${latest_commit_id}.txt requirements.txt
+
+            if [ -d .venv_${latest_commit_id} ]; then
+                mv -T .venv_${latest_commit_id} .venv
+            fi
+        else
+            echo "No matching requirements files or all lines were skipped."
+        fi
 
         cd -
-        echo -e "${license_finder_columns// /,}\n$(cat $license_finder_output_path_relative)" | sudo tee $license_finder_output_path_relative
+        
+        if [[ -f $license_finder_output_path_relative ]]; then
+            echo -e "${license_finder_columns// /,}\n$(cat $license_finder_output_path_relative)" | sudo tee $license_finder_output_path_relative > /dev/null
+        fi
     fi
 
     if [[ $? -ne 0 ]]; then
@@ -56,14 +120,16 @@ generate_report() {
 
     if [ -f $repo_path/package.json ]; then
         license_finder_output_path_relative=reports/$repo_name/tool_outputs/${repo_name}_${latest_commit_id}_npm_license_finder.csv
-        
+
         cd $repo_path/
 
+        echo -e "\n ########### Running licensefinder ########### \n"
+
         license_finder_columns="name version licenses approved package_manager summary install_path"
-        time docker run --rm -v $PWD:/scan -v $devsecops_folder_path/reports/:/reports -v $devsecops_folder_path/configs/:/configs licensefinder/license_finder /bin/bash -lc "cd /scan && npm install && license_finder report --decisions-file=/configs/LicenseFinder/dependency_decisions.yml --format=csv --columns=$license_finder_columns | tail -n +2 | tee /$license_finder_output_path_relative"
+        time docker run --rm -v $PWD:/scan -v $devsecops_folder_path/reports/:/reports -v $devsecops_folder_path/configs/:/configs -v ~/.aws:/root/.aws -v ~/.profile:/profile licensefinder/license_finder /bin/bash -lc "cd /scan && apt update > /dev/null && apt install -y libmysqlclient-dev awscli > /dev/null && source /profile && npm install || echo '' > /dev/null && license_finder report --decisions-file=/configs/LicenseFinder/dependency_decisions.yml --format=csv --columns=$license_finder_columns| grep -v "LicenseFinder::*" | tee /$license_finder_output_path_relative > /dev/null"
 
         cd -
-        echo -e "${license_finder_columns// /,}\n$(cat $license_finder_output_path_relative)" | sudo tee $license_finder_output_path_relative
+        echo -e "${license_finder_columns// /,}\n$(cat $license_finder_output_path_relative)" | sudo tee $license_finder_output_path_relative > /dev/null
     fi
 
     if [[ $? -ne 0 ]]; then
@@ -71,7 +137,7 @@ generate_report() {
         exit 2
     fi
 
-    echo "-- Supply Chain Security report is at reports/$repo_name/${repo_name}_${latest_commit_id}_supply_chain.csv"
+    # echo "-- Supply Chain Security report is at reports/$repo_name/${repo_name}_${latest_commit_id}_supply_chain.csv"
 
     # Bandit is an open-source SAST tool designed specifically for Python applications.
     echo -e "\n ########### Running Bandit ########### \n"
@@ -81,7 +147,7 @@ generate_report() {
     # Horusec is an open-source tool that performs a static code analysis to identify security flaws during development.
     # Current languages for analysis are C#, Java, Kotlin, Python, Ruby, Golang, Terraform, Javascript, Typescript, Kubernetes, PHP, C, HTML, JSON, Dart, Elixir, Shell, and Nginx.
     echo -e "\n ########### Running Horusec ########### \n"
-    time docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v $repo_path:/src -v $(pwd)/reports:/reports horuszup/horusec-cli:v2.9.0-beta.3 horusec start -p /src -P $repo_path -i="**/node_modules/**, **/venv/**, **/tests/**, **db.sqlite3**" -o json -O /reports/$repo_name/tool_outputs/${repo_name}_${latest_commit_id}_horusec.json
+    time docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v $repo_path:/src -v $(pwd)/reports:/reports horuszup/horusec-cli:v2.9.0-beta.3 horusec start -p /src -P $repo_path -i="**/node_modules/**, **/venv/**, **/tests/**, **db.sqlite3**, dist/**" -o json -O /reports/$repo_name/tool_outputs/${repo_name}_${latest_commit_id}_horusec.json
 
     # Bearer is a static application security testing (SAST) tool that scans your source code and analyzes your data flows to discover, filter, and prioritize security and privacy risks.
     # Currently supports JavaScript, TypeScript, Ruby, and Java stacks.
